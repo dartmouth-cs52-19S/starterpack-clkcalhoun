51d2740312076acaba327ea10bafe6ab4e0fa4fc
Craig <clkcalhoun@gmail.com>
2019-04-11 13:46:05 -0400

    after surge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import './style.scss';
+import $ from 'jquery';
+
+let num = 0;
+
+function countFunction() {
+  num += 1;
+  $('#main').html(`You have been on this page for ${num} seconds!`);
+}
+
+setInterval(countFunction, 1000);
